8e85e57818af40218e211b6e38d8967656d29b56
ken88ling <ken88ling@gmail.com>
2016-08-05 21:50:05 +1000

    initialCreate

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+//running at command
+//app.get('/',
+//    function(req, res) {
+//        res.send("Hello world from server.js ");
+//    });
+
+app.listen(3000);
+console.log("Server running on port 3000");
+
+
+//var http = require('http');
+//var port = process.env.port || 1337;
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
