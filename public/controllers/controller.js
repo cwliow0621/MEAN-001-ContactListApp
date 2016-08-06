0feab09b546d980c5ffe376e3e962bba62d33220
ken88ling <ken88ling@gmail.com>
2016-08-07 00:24:07 +1000

    edit function

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,15 +3,35 @@ myApp.controller('AppCtrl', ['$scope', '$http',
     function($scope, $http) {
         console.log("Hello World from controller");
 
-        $http.get('/contactlist')
-            .success(function(response) {
-                console.log("I got the data I requested");
-                $scope.contactlist = response;
+        var refresh = function() {
+            $http.get('/contactlist').success(function(response) {
+                    console.log("I got the data I requested");
+                    $scope.contactlist = response;
+                    $scope.contact = "";
             });
+        };
+
+        refresh();
 
         $scope.addContact = function() {
             console.log($scope.contact);
-            $http.post('/contactlist', $scope.contact);
+            $http.post('/contactlist', $scope.contact).success(function (response) {
+                console.log(response);
+                    refresh();
+                });
         };
 
+        $scope.remove = function(id) {
+            console.log(id);
+            $http.delete('/contactlist/' + id).success(function(response) {
+                    refresh();
+                });
+        };
+
+        $scope.edit = function(id) {
+            console.log(id);
+            $http.get('/contactlist/' + id).success(function(response) {
+                    $scope.contact = response;
+                });
+        };
     }]);
--- server.js
+++ server.js
@@ -2,34 +2,43 @@
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-app.get('/contactlist',
-    function(req, res) {
+app.get('/contactlist', function(req, res) {
         db.contactlist.find(function(err, docs) {
             console.log(docs);
             res.json(docs);
         });
     });
 
-app.post('/contactlist',
-    function(req, res) {
+app.post('/contactlist', function(req, res) {
         console.log(req.body);
+        db.contactlist.insert(req.body,function(err, doc) {
+                res.json(doc);
+            });
     });
-//running at command
-//app.get('/',
-//    function(req, res) {
-//        res.send("Hello world from server.js ");
-//    });
 
+app.delete('/contactlist/:id', function(req, res) {
+        var id = req.params.id;
+        console.log(id);
+        db.contactlist.remove({ _id: mongojs.ObjectId(id) },
+            function(err, doc) {
+                res.json(doc);
+            });
+    });
+
+app.get('/contactlist/:id', function(req, res) {
+        var id = req.params.id;
+        console.log(id);
+        db.contactlist.findOne({ _id: mongojs.ObjectId(id) },
+            function(err, doc) {
+                res.json(doc);
+            });
+    });
+    
 app.listen(3000);
 console.log("Server running on port 3000");
 
-
-//var http = require('http');
-//var port = process.env.port || 1337;
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//}).listen(port);
