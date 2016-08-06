6964fd07ed08665fb36729d7bae580391c9713e4
ken88ling <ken88ling@gmail.com>
2016-08-06 16:29:52 +1000

    add Mongojs

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 ﻿var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 app.use(express.static(__dirname + "/public"));
+
+app.get('/contactlist',
+    function(req, res) {
+        console.log("I received a Get request");
+        person1 = {
+            name: 'Tim',
+            email: 'kenling@gmail.com',
+            number: '(111) 111-1111'
+        };
+
+        person2 = {
+            name: 'Emily',
+            email: 'emily@gmail.com',
+            number: '(222) 222-2222'
+        };
+
+        person3 = {
+            name: 'John',
+            email: 'john@email.com',
+            number: '(333) 333-3333'
+        };
+        var contactlist = [person1, person2, person3];
+        res.json(contactlist);
+    });
 //running at command
 //app.get('/',
 //    function(req, res) {
