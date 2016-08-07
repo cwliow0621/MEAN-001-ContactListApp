60856d12b7e348aac81450d24d7d38fc8a29f5d2
ken88ling <ken88ling@gmail.com>
2016-08-07 11:25:50 +1000

    update-deselect

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -34,4 +34,16 @@ myApp.controller('AppCtrl', ['$scope', '$http',
                     $scope.contact = response;
                 });
         };
+
+        $scope.update = function() {
+            console.log($scope.contact._id);
+            $http.put('/contactlist/' + $scope.contact._id, $scope.contact)
+                .success(function(response) {
+                    refresh();
+                });
+        };
+
+        $scope.deselect = function() {
+            $scope.contact = "";
+        }
     }]);
